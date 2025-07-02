9912aabde89ea0914c8f0e62a8a63fba014c593b
JYuan <5726271+jyuan86@user.noreply.gitee.com>
2025-07-02 10:10:56 +0800

    feat: 添加study文件

---
+++ _study/app/src/app/glsl-shaders-1/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styles from "./styles.module.css";
+
+function App() {
+  return <div>123456789</div>;
+}
+
+export default () => {
+  return (
+    <div className={styles.main}>
+      <App />
+    </div>
+  );
+};
